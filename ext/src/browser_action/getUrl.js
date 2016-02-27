8816b19db4f58527aaf1141a3476a4e6e90b09a0
Pat Myron <pmyron09@gmail.com>
2016-02-27 14:23:47 -0500

    fix manifest, style popup, and get first redirect

--- ext/src/browser_action/getUrl.js
+++ ext/src/browser_action/getUrl.js
@@ -1,16 +1,34 @@
 function getUrl()
 {
-    return "http://www.google.com";
+	var domainName = "";
+	chrome.tabs.query({ //This method output active URL 
+		"active": true,
+		"currentWindow": true,
+		"status": "complete",
+		"windowType": "normal"
+	}, function (tabs) {
+		for (tab in tabs) {
+			domainName = tabs[tab].url;
+			domainName = domainName.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+			function getDomainName(hostName)
+			{
+				return hostName.substring(hostName.lastIndexOf(".", hostName.lastIndexOf(".") - 1) + 1);
+			}
+			var fullDomain = getDomainName(domainName);
+			var company = fullDomain.split(".")[0];
+			var newURL = "https://www.raise.com/buy-gift-cards?utf8=%E2%9C%93&keywords="+company+"&type=electronic";
+        	chrome.tabs.create({ url: newURL });
+		}
+	});
 }
 
 function clickHandler(e) {
-    chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
+	chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
         this.close(); // close the popup when the background finishes processing request
-var newURL = "http://stackoverflow.com/";
-  chrome.tabs.create({ url: newURL });
-      });
+        getUrl();
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('button').addEventListener('click', clickHandler);
+	document.getElementById('button').addEventListener('click', clickHandler);
 })
