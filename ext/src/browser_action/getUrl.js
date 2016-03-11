cb2633a871ab95808e568e23bc823328055c8c3f
Pat Myron <pmyron09@gmail.com>
2016-03-11 12:45:58 -0800

    refactoring

--- ext/src/browser_action/getUrl.js
+++ ext/src/browser_action/getUrl.js
@@ -1,3 +1,14 @@
+function getCompanyName(tabs, tab) {
+	domainName = tabs[tab].url;
+	domainName = domainName.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+	function getDomainName(hostName)
+	{
+		return hostName.substring(hostName.lastIndexOf(".", hostName.lastIndexOf(".") - 1) + 1);
+	}
+	var fullDomain = getDomainName(domainName);
+	return fullDomain.split(".")[0];
+}
+
 function getUrl()
 {
 	var inputtedPurchasePrice = document.getElementById('purchasePriceTextBox').value
@@ -10,14 +21,8 @@ function getUrl()
 		"windowType": "normal"
 	}, function (tabs) {
 		for (tab in tabs) {
-			domainName = tabs[tab].url;
-			domainName = domainName.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-			function getDomainName(hostName)
-			{
-				return hostName.substring(hostName.lastIndexOf(".", hostName.lastIndexOf(".") - 1) + 1);
-			}
-			var fullDomain = getDomainName(domainName);
-			var company = fullDomain.split(".")[0];
+			var company = getCompanyName(tabs, tab);
+
 			var url1 = "https://www.raise.com/buy-gift-cards?utf8=%E2%9C%93&keywords="+company+"&type=electronic";
 			
 			var url2 = "";
