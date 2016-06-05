77560b1dcbdec4d82967f106850fdcdf4705c03b
Pat Myron <pmyron09@gmail.com>
2016-06-05 14:09:58 -0700

    google analytics

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -4,6 +4,15 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-58135639-4']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
