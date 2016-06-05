12a50e9deecce2931bcc74dcfb55464e596b9ec7
Pat Myron <pmyron09@gmail.com>
2016-06-05 14:45:12 -0700

    trying to track events

--- ext/src/ga.js
+++ ext/src/ga.js
@@ -3,7 +3,18 @@ _gaq.push(['_setAccount', 'UA-58135639-4']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+function trackButton(e) {
+	_gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+
+window.onload = function () {
+	var buttons = document.querySelectorAll('button');
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', trackButton);
+	}
+}
